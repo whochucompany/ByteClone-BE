46f2ac9ba442682ae6128dd728d92c57fe1c1ce6
jobmania <xzxz7003@naver.com>
2022-08-24 17:31:37 +0900

    뉴스 레디스, 뉴스 레디스 레포를 넣었을 때 스타트 정상

--- src/main/java/com/whochucompany/byteclone/redis/NewsRedisRepository.java
+++ src/main/java/com/whochucompany/byteclone/redis/NewsRedisRepository.java
@@ -2,6 +2,6 @@ package com.whochucompany.byteclone.redis;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshRedisRepository extends CrudRepository<RefreshRedisToken, Long> {
+public interface NewsRedisRepository extends CrudRepository<NewsRedis, String> {
 
 }
